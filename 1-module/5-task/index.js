cb4f00388446ae5483537e0b182c6f34a119d499
Marina Medvedeva <ms.opiate@mail.ru>
2023-10-04 22:59:25 +0300

    Task №1.5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,8 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (maxlength < str.length) {
+    let resultStr = str.slice(0, maxlength - 1);
+    return resultStr + '…';
+  } else {
+    return str;
+  }
 }
