6624df522d98d2095193741ab09a0ebf1b6647c7
Marina Medvedeva <ms.opiate@mail.ru>
2023-10-16 22:48:35 +0300

    Task №3.3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('');
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i - 1] === '-' ) {
+      result.push(arr[i].toUpperCase());
+    } else if (arr[i]  !== '-') {
+      result.push(arr[i])
+    }
+  }; 
+
+  return result.join('');
 }
