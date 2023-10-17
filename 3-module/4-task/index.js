2d65ce1d94bdc8548e724cf53ad9792aff15bfd9
Marina Medvedeva <ms.opiate@mail.ru>
2023-10-17 21:29:04 +0300

    Upd

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
-  // ваш код...
+  let salaryUsers = users.filter(user => user.age <= age).map(salaryUser => salaryUser.name + ', ' + salaryUser.balance);
+
+  let str = salaryUsers.join('\n');
+  return str;
 }
