7a64fd7e780bebe095c6a9b156911d2212f4fd55
Marina Medvedeva <ms.opiate@mail.ru>
2023-10-28 21:43:26 +0300

    Upd

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel');
+  const carouselRow = document.querySelector('.carousel__inner');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const img = document.querySelector('.carousel__img');
+  const slideWidth = img.offsetWidth;
+  const slides = document.querySelectorAll('.carousel__slide');
+  const counter = 1;
+
+  arrowLeft.style.display = 'none';
+
+  carousel.addEventListener('click', function(evt) {  
+    let target = evt.target;
+
+    if (counter <= slides.length) {
+      if (target !==  arrowLeft && target !==  arrowRight && target.parentElement !== arrowLeft && target.parentElement !== arrowRight) {
+        return;
+      } else if (target === arrowLeft || target.parentElement === arrowLeft) {
+        counter--;
+        carouselRow.style.transform = `translateX(-${slideWidth * counter - slideWidth}px)`;
+        
+      } else if (target === arrowRight || target.parentElement === arrowRight) {
+        carouselRow.style.transform = `translateX(-${slideWidth * counter}px)`;
+        arrowLeft.style.display = '';
+        counter++;
+      }
+    }
+
+  if (counter === slides.length) {
+    arrowRight.style.display = 'none'
+  } else if (counter === 1) {
+    arrowLeft.style.display = 'none';
+    arrowRight.style.display = '';
+  }
+  })
 }
